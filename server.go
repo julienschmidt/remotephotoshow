f9528744945610ae4f80e4d40f757bad8d44199a
Julien Schmidt <github@julienschmidt.com>
2015-01-04 12:30:05 +0800

    Handle progress server-side

--- server.go
+++ server.go
@@ -9,10 +9,12 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 )
 
 // Set your config here
@@ -24,27 +26,35 @@ const (
 	photoDir string = "./photos/"
 )
 
+var (
+	broker    *Broker
+	imgID     uint
+	endID     uint
+	photoJSON []byte
+	photoErr  error
+)
+
 type Broker struct {
 	// Events are pushed to this channel by the main events-gathering routine
-	Notifier chan []byte
+	Notifier chan string
 
 	// New client connections
-	newClients chan chan []byte
+	newClients chan chan string
 
 	// Closed client connections
-	closingClients chan chan []byte
+	closingClients chan chan string
 
 	// Client connections registry
-	clients map[chan []byte]bool
+	clients map[chan string]bool
 }
 
 func NewServer() (broker *Broker) {
 	// Instantiate a broker
 	broker = &Broker{
-		Notifier:       make(chan []byte, 1),
-		newClients:     make(chan chan []byte),
-		closingClients: make(chan chan []byte),
-		clients:        make(map[chan []byte]bool),
+		Notifier:       make(chan string, 1),
+		newClients:     make(chan chan string),
+		closingClients: make(chan chan string),
+		clients:        make(map[chan string]bool),
 	}
 
 	// Set it running - listening and broadcasting events
@@ -68,7 +78,7 @@ func (broker *Broker) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 
 	// Each connection registers its own message channel with the Broker's
 	// connections registry
-	messageChan := make(chan []byte)
+	messageChan := make(chan string)
 
 	// Signal the broker that we have a new connection
 	broker.newClients <- messageChan
@@ -114,6 +124,7 @@ func (broker *Broker) listen() {
 		case event := <-broker.Notifier:
 			// We got a new event from the outside!
 			// Send event to all connected clients
+			fmt.Println("Send event:", event)
 			for clientMessageChan, _ := range broker.clients {
 				clientMessageChan <- event
 			}
@@ -121,26 +132,40 @@ func (broker *Broker) listen() {
 	}
 }
 
-func handlePhotos(rw http.ResponseWriter, req *http.Request) {
+func reset() {
+	imgID = 0
+	photoJSON, photoErr = loadPhotos()
+	broker.Notifier <- "r"
+}
+
+func setID(id uint) error {
+	if id > endID {
+		errors.New("Invalid ID")
+	}
+
+	imgID = id
+	broker.Notifier <- fmt.Sprintf("s%d", id)
+
+	return nil
+}
+
+func loadPhotos() ([]byte, error) {
 	dir, err := os.Open(photoDir)
 	if err != nil {
-		http.Error(rw, err.Error(), http.StatusInternalServerError)
-		return
+		return nil, err
 	}
 	defer dir.Close()
 
 	fi, err := dir.Stat()
 	if err != nil {
-		http.Error(rw, err.Error(), http.StatusInternalServerError)
-		return
+		return nil, err
 	}
 
 	filenames := make([]string, 0)
 	if fi.IsDir() {
 		fis, err := dir.Readdir(-1) // -1 means return all the FileInfos
 		if err != nil {
-			http.Error(rw, err.Error(), http.StatusInternalServerError)
-			return
+			return nil, err
 		}
 
 		for _, fileinfo := range fis {
@@ -150,20 +175,36 @@ func handlePhotos(rw http.ResponseWriter, req *http.Request) {
 		}
 	}
 
+	return json.Marshal(filenames)
+}
+
+func handlePhotos(rw http.ResponseWriter, req *http.Request) {
+	if photoErr != nil {
+		http.Error(rw, photoErr.Error(), http.StatusInternalServerError)
+		return
+	}
+
 	rw.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(rw).Encode(filenames)
+	rw.Header().Set("Cache-Control", "no-cache")
+	fmt.Fprintf(rw, `{"photos": %s, "id": %d}`, photoJSON, imgID)
 }
 
 func handleCMD(rw http.ResponseWriter, req *http.Request) {
-	switch req.FormValue("cmd") {
-	case "prev":
-		log.Println("Sending event: prev")
-		broker.Notifier <- []byte("prev")
+	switch req.PostFormValue("cmd") {
+	case "set":
+		id, err := strconv.ParseUint(req.PostFormValue("id"), 10, 0)
+
+		if err == nil {
+			err = setID(uint(id))
+		}
+
+		if err != nil {
+			http.Error(rw, err.Error(), http.StatusBadRequest)
+		}
 		return
 
-	case "next":
-		log.Println("Sending event: next")
-		broker.Notifier <- []byte("next")
+	case "reset":
+		reset()
 		return
 
 	default:
@@ -172,12 +213,12 @@ func handleCMD(rw http.ResponseWriter, req *http.Request) {
 	}
 }
 
-var broker *Broker
-
 func main() {
 	// SSE client broker
 	broker = NewServer()
 	http.Handle("/listen", broker)
+
+	reset()
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "remotephoto.html")
@@ -193,7 +234,6 @@ func main() {
 		default:
 			http.Error(w, "Method not allowed!", http.StatusMethodNotAllowed)
 		}
-
 	})
 
 	http.HandleFunc("/assets/", func(w http.ResponseWriter, r *http.Request) {
